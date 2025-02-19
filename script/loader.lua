5b5e4773c2b92c7d1624a47d786d0cbd3d1cb02f
folxyys <91986184+folxxs@users.noreply.github.com>
2025-02-18 22:08:32 -0700

    Update loader.lua

--- script/loader.lua
+++ script/loader.lua
@@ -1,5 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vKhonshu/intro2/main/ui2"))() 
-local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vKhonshu/intro/main/ui"))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
 -- Get the executor name
 local executorname = getexecutorname() or identifyexecutor() or "Unknown Executor"
@@ -9,9 +8,20 @@ local supportedExecutors = { AWP = true, Wave = true, Nihon = true, ["Synapse Z"
 
 -- Check if executor is supported
 if supportedExecutors[executorname] then
-    NotifyLib.prompt('Executor Supported', executorname .. " is supported!", 5)
+    Fluent:Notify({
+        Title = "Another Hub",
+        Content = executorname .. " is supported!",
+        SubContent = "Valid", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+    })
+    
     loadstring(game:HttpGet('https://raw.githubusercontent.com/folxxs/another-hub/refs/heads/main/main/loader.lua'))()
 else
-    NotifyLib.prompt('Executor Not Supported', executorname .. " is not supported, expect errors.", 5)
+    Fluent:Notify({
+        Title = "Another Hub",
+        Content = executorname .. " is not supported, expect errors.",
+        SubContent = "Failed", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+    })
     loadstring(game:HttpGet('https://raw.githubusercontent.com/folxxs/another-hub/refs/heads/main/main/loader.lua'))()
 end
